241852569246fae55d78de594ccf7eca151b5850
DemonAITYX <nabilakbarsss074@gmail.com>
2026-06-03 15:12:48 +0700

    table removed and layout update

--- resources/views/components/layouts/dashboard.blade.php
+++ resources/views/components/layouts/dashboard.blade.php
@@ -491,35 +491,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     Distribusi Bantuan
 
-                    </a>
-
-                    <!-- JADWAL SAYA -->
-                    <a
-                    data-menu="jadwal saya my schedule" 
-                    href="#"
-                    class="flex items-center gap-4 px-5 py-4 rounded-2xl transition-all duration-300
-
-                    {{ request()->is('volunteer/my-schedule')
-                        ? 'bg-green-500 text-white shadow-lg'
-                        : 'text-gray-600 hover:bg-green-100 hover:text-green-700'
-                    }}">
-
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.8"
-                    stroke="currentColor"
-                    class="w-5 h-5">
-
-                    <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M6.75 3v2.25m10.5-2.25v2.25M3 18.75V7.5A2.25 2.25 0 0 1 5.25 5.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25A2.25 2.25 0 0 1 18.75 21H5.25A2.25 2.25 0 0 1 3 18.75Zm0-10.5h18"/>
-
-                    </svg>
-                    Jadwal Saya
-
-                    </a>
+                </a>
 
                     <!-- PROFIL -->
                     <a

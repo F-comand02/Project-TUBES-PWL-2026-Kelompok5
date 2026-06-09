11c69203e854e33c2cb8fceb83265157525d2050
F-comand02 <marunoyumitgc@gmail.com>
2026-06-10 00:22:01 +0700

    Update Login Admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -30,6 +30,10 @@ class DatabaseSeeder extends Seeder
             ]
         );
         
+        $this->call([
+        RoleSeeder::class,
+        AdminUserSeeder::class,
+        ]);
     }
 
     
